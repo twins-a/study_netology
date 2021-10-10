6a6a72c8d25b813ae0e7d89ee6251eebd73af045
Александр С. Тульев <atulyev@kivach.local>
2021-10-10 22:16:50 +0300

    Add files HomeWork Cloud Functions

---
+++ CLD/HomeWork Cloud Functions/main.py
@@ -0,0 +1,8 @@
+def hello_gcs(event, context):
+    """Triggered by a change to a Cloud Storage bucket.
+    Args:
+         event (dict): Event payload.
+         context (google.cloud.functions.Context): Metadata for the event.
+    """
+    file = event
+    print(f"Processing file: {file['name']}. Time created file: {file['timeCreated']}. Time updated: {file['updated']}.")
